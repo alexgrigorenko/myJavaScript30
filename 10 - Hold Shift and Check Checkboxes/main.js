4ee13d888d4f3ad072ad24416f5718be9e1b1d10
Alex <alexey.grygorenko@gmail.com>
2019-02-19 10:32:40 +0200

    add exercise 10

---
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -0,0 +1,30 @@
+// Get all input element with type="checkbox" inside 'inbox' class.
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+  // Check if they had the shift key down
+  // AND check that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('Starting to check them in between!');
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this; // Store last checked element in the lastChecked variable.
+}
+
+// addEventListener to all checkbox element.
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
